3470729afd61882c69155bd270e9b727094042e9
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-04-29 10:53:05 -0400

    cambios en el script

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,45 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Función para reducir el tamaño de la barra de navegación
+  var minimiza = function () {
+    const colapzar = document.body.querySelector("#mainNav");
+    if (!colapzar) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      colapzar.classList.remove("navbar-shrink");
+    } else {
+      colapzar.classList.add("navbar-shrink");
+    }
+  };
 
-window.addEventListener('DOMContentLoaded', event => {
+  // Reducir el tamaño de la barra de navegación
+  minimiza();
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+  // Reducir el tamaño de la barra de navegación cuando se desplaza la página
+  document.addEventListener("scroll", minimiza);
 
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  // Activate Bootstrap scrollspy on the main nav element
+  const navegacion = document.body.querySelector("#mainNav");
+  if (navegacion) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      rootMargin: "0px 0px -40%",
     });
+  }
 
+  // Colapsar la barra de navegación responsive cuando el botón de alternancia (toggler) está visible
+  const hamburguesa = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(hamburguesa).display !== "none") {
+        hamburguesa.click();
+      }
+    });
+  });
+
+  //Ver dato ingresado en el input y comparar con el json y si ambos coincides crear un div
+  
 });
